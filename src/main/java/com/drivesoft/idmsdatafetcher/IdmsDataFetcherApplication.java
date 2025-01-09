2e4c9b2aab7ac73c5ee4903f2e7c5e3cc1033c69
Syed <syedali3334@gmail.cojm>
2025-01-09 17:32:50 +0530

    Adding project to git

---
+++ src/main/java/com/drivesoft/idmsdatafetcher/IdmsDataFetcherApplication.java
@@ -0,0 +1,13 @@
+package com.drivesoft.idmsdatafetcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdmsDataFetcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdmsDataFetcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/drivesoft/idmsdatafetcher/IdmsDataFetcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drivesoft.idmsdatafetcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdmsDataFetcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
